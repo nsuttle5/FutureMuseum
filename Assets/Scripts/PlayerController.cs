b25e9befc009c6e3b5f3d59431c6186c7afb2a2f
nsuttle5 <nsuttles1754@gmail.com>
2024-11-27 00:57:58 -0500

    Messed with lighting and attempted to fix wall glitch

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,7 @@ public class PlayerController : MonoBehaviour
         Cursor.visible = false;
     }
 
-    public GameObject player;
+    public Rigidbody playerRigidbody;
     public float speed = 5.0f;
     public float sprintSpeed = 10.0f;
     public float mouseSensitivity = 100.0f;
@@ -17,6 +17,17 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        // Handle camera rotation
+        rotationX += Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        rotationY -= Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        rotationY = Mathf.Clamp(rotationY, -90.0f, 90.0f);
+
+        Camera.main.transform.localRotation = Quaternion.Euler(rotationY, rotationX, 0.0f);
+    }
+
+    // FixedUpdate is called once per physics frame
+    void FixedUpdate()
     {
         // Handle player movement
         float moveHorizontal = Input.GetAxis("Horizontal");
@@ -35,13 +46,6 @@ public class PlayerController : MonoBehaviour
         float currentSpeed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : speed;
 
         Vector3 movement = forward * moveVertical + right * moveHorizontal;
-        player.transform.Translate(movement * currentSpeed * Time.deltaTime, Space.World);
-
-        // Handle camera rotation
-        rotationX += Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        rotationY -= Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-        rotationY = Mathf.Clamp(rotationY, -90.0f, 90.0f);
-
-        Camera.main.transform.localRotation = Quaternion.Euler(rotationY, rotationX, 0.0f);
+        playerRigidbody.MovePosition(playerRigidbody.position + movement * currentSpeed * Time.fixedDeltaTime);
     }
 }
